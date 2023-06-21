9dafe72bd4e24a59948b1294d84d817671d06a6d
william-789 <riberackw@gmail.com>
2023-06-21 12:22:21 +0100

    feat: wall has collision and hero can move

--- src/game/engine.js
+++ src/game/engine.js
@@ -4,46 +4,57 @@ import Hero from "../objects/hero.js";
 import Interface from "./interface.js";
 import Room from "./room.js";
 
-import room00 from "../../rooms/room0.js";//delete
+import room00 from "../../rooms/room0.js"; //delete
 
 import Direction from "../util/direction.js";
 
 class Engine {
-    gui = Interface.getInstance();
-    hero;
-    rooms = []
+  gui = Interface.getInstance();
+  hero;
+  rooms = [];
 
-    init() {
-        console.log("Engine init");
+  init() {
+    console.log("Engine init");
 
-        let floorTiles = [];
-        for(let x = 0; x < 10; x++) {
-            for(let y = 0; y < 10; y++) {
-                let position = new Position(x, y);
-                floorTiles.push(new Floor(position));
-            }
-        }
-        this.gui.addImages(floorTiles);
-
-        //let fireball = new FireBall(new Position(5, 3), Direction.RIGHT);
-        //this.gui.addImage(fireball);
-        //fireball.start();
-        const room0 = new Room(room00, "room0");
-        let roomObjects = room0.readPattern();
-        this.rooms.push(roomObjects);
-        console.log(roomObjects);
-        this.gui.addImages(roomObjects);
-
-        this.hero = new Hero(new Position(5, 5));
-        this.gui.addImage(this.hero);
-
-        this.gui.start();
+    let floorTiles = [];
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
+        let position = new Position(x, y);
+        floorTiles.push(new Floor(position));
+      }
     }
+    this.gui.addImages(floorTiles);
 
-    keyPressed(key) {
-        console.log("User pressed key", key);
-        //TODO
+    //let fireball = new FireBall(new Position(5, 3), Direction.RIGHT);
+    //this.gui.addImage(fireball);
+    //fireball.start();
+    const room0 = new Room(room00, "room0");
+    let roomObjects = room0.readPattern();
+    this.rooms.push(roomObjects);
+    console.log(roomObjects);
+    this.gui.addImages(roomObjects);
+
+    this.hero = new Hero(room0.heroPosition);
+    this.gui.addImage(this.hero);
+
+    this.gui.start();
+  }
+
+  keyPressed(key) {
+    let newK = key.replace(/arrow/i, "").toUpperCase();
+    let vector = Direction[newK].asVector();
+    let nextPosition = this.hero.position.plus(vector);
+    let objectInRoom = this.rooms[0].find(
+      (object) =>
+        object.position.x === nextPosition.x &&
+        object.position.y === nextPosition.y
+    );
+    //move Hero if there's no wall
+    if (!objectInRoom || !objectInRoom.collision) {
+      this.hero.position = nextPosition;
+      this.gui.update();
     }
+  }
 }
 
 export default Engine;
