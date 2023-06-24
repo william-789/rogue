23851fb7d2bd4605aab682410d8e3a06ebb25d2c
william-789 <riberackw@gmail.com>
2023-06-24 17:11:08 +0100

    refactor: hero has props previously in StatusBar

--- src/objects/hero.js
+++ src/objects/hero.js
@@ -1,20 +1,35 @@
 import Character from "./Character.js";
+import FireBall from "../objects/fireBall.js";
+import Meat from "../objects/meat.js";
+import Position from "../util/position.js";
 
 class Hero extends Character {
+  fireBalls;
+  items = [];
 
-  // static #instance;
-  // static getInstance() {
-  //     if(Hero.#instance === undefined) {
-  //         Hero.#instance = new Hero();
-  //     }
-  //     return Hero.#instance;
-  // }
   constructor(position) {
     super(position);
+    this.health = 4;
+    this.fireBalls = [
+      new FireBall(new Position(0,0)),
+      new FireBall(new Position(1,0)),
+      new FireBall(new Position(2,0))
+    ];
+    this.items = [
+      new Meat(new Position(7,0)),
+      new Meat(new Position(8,0)),
+      new Meat(new Position(9,0))
+    ];
   }
 
   get image() {
     return "Hero.png";
+  }
+
+  pickUpItem() {}
+
+  dropItem(delIndex) {
+    this.items.splice(delIndex,1);
   }
 }
 
