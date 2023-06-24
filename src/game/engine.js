ce6427adf920146003d277e6326fd8167bb20136
william-789 <riberackw@gmail.com>
2023-06-24 18:03:15 +0100

    refactor: get current room from room list

--- src/game/engine.js
+++ src/game/engine.js
@@ -4,11 +4,16 @@ import Hero from "../objects/hero.js";
 import Interface from "./interface.js";
 import Room from "./room.js";
 
-import room00 from "../../rooms/room0.js"; //delete
-
 import Direction from "../util/direction.js";
 import MovementController from "./movementController.js";
 import StatusBar from "./statusBar.js";
+
+//Room string patterns imports
+import room00 from "../../rooms/room0.js";
+import room01 from "../../rooms/room1.js";
+import room02 from "../../rooms/room2.js";
+
+const roomPatterns = [room00,room01,room02];
 
 class Engine {
   gui = Interface.getInstance();
@@ -21,6 +26,12 @@ class Engine {
   init() {
     console.log("Engine init");
 
+    // Create all rooms and store them
+    for(let pattern of roomPatterns) {
+      const room = new Room(pattern, `room${this.rooms.length}`);
+      this.rooms.push(room);
+    }
+    // Make floor
     let floorTiles = [];
     for (let x = 0; x < 10; x++) {
       for (let y = 0; y < 10; y++) {
@@ -33,16 +44,15 @@ class Engine {
     //let fireball = new FireBall(new Position(5, 3), Direction.RIGHT);
     //this.gui.addImage(fireball);
     //fireball.start();
-    const room0 = new Room(room00, "room0");
-    let roomObjects = room0.readPattern();
-    this.rooms.push(roomObjects);
-    this.currentRoom = room0;//change
-    this.gui.addImages(roomObjects);
 
-    this.hero = new Hero(room0.heroPosition);
+    // Set current room and add to gui
+    this.currentRoom = this.rooms[0];//change
+    this.gui.addImages(this.currentRoom.readPattern());
+
+    this.hero = new Hero(this.currentRoom.heroPosition);
     this.gui.addImage(this.hero);
 
-    this.mControl = new MovementController(this.hero, room0.enemies, roomObjects);
+    this.mControl = new MovementController(this.hero);
 
     this.statusBar.init(this.hero);
 
@@ -64,7 +74,11 @@ class Engine {
     } else {
       let newK = key.replace(/arrow/i, "").toUpperCase();
       let vector = Direction[newK].asVector();
-      this.mControl.handleMovement(vector);
+      try {
+        this.mControl.handleMovement(vector, this.currentRoom);
+      } catch (e) {
+        console.log("Error:", e.message);
+      }
     }
 
   }
