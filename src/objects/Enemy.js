b744a5861a324a6591faa026d736085a600e1993
william-789 <riberackw@gmail.com>
2023-06-29 13:28:28 +0100

    refactor: add comments

--- src/objects/Enemy.js
+++ src/objects/Enemy.js
@@ -2,24 +2,25 @@ import Character from "./Character.js";
 import Direction from "../util/direction.js";
 
 class Enemy extends Character {
-  static closeDistSquared = 5; // Min (2,1) or (1,2)
+  static closeDistSquared = 5; // Minimum squared distance to consider the hero close
   distToHeroSq;
   constructor(position) {
     super(position);
   }
 
-  // set next position
+  // Set the next position for the enemy based on the hero's position
   move(heroPos) {
     let close = this.distToHeroSq <= Enemy.closeDistSquared;
-    console.log("Is hero close?", close);
     if(close) this.chaseHero(heroPos);
     else this.randomMove();
   }
 
+  // Calculate the squared distance between the enemy and the hero
   getDistToHero(heroPosition) {
     this.distToHeroSq = (this.position.x - heroPosition.x)**2 + (this.position.y - heroPosition.y)**2;
   }
 
+  // Move the enemy randomly in any direction
   randomMove() {
     const movementOptions = ["UP", "RIGHT", "LEFT", "DOWN"];
     const randomIndex = Math.floor(Math.random()*movementOptions.length);
@@ -28,10 +29,11 @@ class Enemy extends Character {
     this.nextPosition = this.position.plus(vector);
   }
 
+  // Chase the hero by moving towards their position
   chaseHero(heroPos) {
     const distX = heroPos.x - this.position.x;
     const distY = heroPos.y - this.position.y;
-    //checks larger dist to define orientation of movement
+    // Check the larger distance to define the orientation of movement
     if (Math.abs(distX) > Math.abs(distY)) {
       if (distX < 0) {
         this.nextPosition = this.position.plus(Direction.LEFT.asVector());
